60d8b606043ba020f7d2a6d927e0e3b16638a0ef
Leonardo~ <140676545+leonardo46843@users.noreply.github.com>
2025-02-28 10:53:10 +0700

    Adding Product, Search & Cart activation, resolve null href bug.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,44 @@
-// Toogle Activation
+// Toogle Activation Hamburger
 const sideBar = document.querySelector('nav');
 
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
   sideBar.classList.toggle('active');
+  e.preventDefault();
+};
+
+// Shopping Cart
+const shoppingCart = document.querySelector('.cart');
+
+document.querySelector('#shopping-cart').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+// Toogle Activation Search
+const searchColumn = document.querySelector('.search-column');
+const searchForm = document.querySelector('#search-form');
+
+document.querySelector('#magnify-search').onclick = (e) => {
+  searchColumn.classList.toggle('active');
+  searchForm.focus();
+  e.preventDefault();
 };
 
 // Close outSide
 const hamburger = document.querySelector('#hamburger-menu');
+const search = document.querySelector('#magnify-search');
+const cart = document.querySelector('#shopping-cart');
 
 document.addEventListener('click', function (e) {
   if (!hamburger.contains(e.target) && !sideBar.contains(e.target)) {
     sideBar.classList.remove('active');
+  }
+
+  if (!search.contains(e.target) && !searchColumn.contains(e.target)) {
+    searchColumn.classList.remove('active');
+  }
+
+  if (!cart.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
   }
 });
